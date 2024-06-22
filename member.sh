1d1a7fa43c811910991837381dc2bef70205c294
Alkhanet <92195275+alkhanet26@users.noreply.github.com>
2024-06-22 19:56:48 +0700

    Update member.sh

--- member.sh
+++ member.sh
@@ -16,6 +16,6 @@ JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)
 echo "-------------------------------"
 echo "Jumlah akun: $JUMLAH user"
 echo "-------------------------------"
-echo "   POWERED BY PRINCENEWBIE.    "
-echo "    COPYRIGHT ONEPIECEVPN      "
+echo "         POWERED BY ALK.       "
+echo "    COPYRIGHT ALKHANETVPN      "
 echo "-------------------------------"
